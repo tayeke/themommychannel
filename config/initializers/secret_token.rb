d87ebebcecba8107b1c84ef4372596c38c9b2be6
Taylor j. Eke <tayloreke@gmail.com>
2012-12-27 13:10:46 -0800

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mommychannel::Application.config.secret_token = '11b11c426d317d9884f599bbe71383008fa363ca089773e08ae1469ce9e774de85e12b74e0e4b587b9e74952aa9eabadcc0606be2b1f2789d42e935cf1817062'
