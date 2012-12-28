fd4e4f554c0325545290b3cdd9af3c84566d6099
Taylor j. Eke <tayloreke@gmail.com>
2012-12-27 21:11:44 -0800

    did some more configuration for heroku

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -1,4 +1,4 @@
-class Admin < ActiveRecord::Base
+class Administrator < ActiveRecord::Base
   has_and_belongs_to_many :users, :join_table => :users_admins
   belongs_to :resource, :polymorphic => true
   
